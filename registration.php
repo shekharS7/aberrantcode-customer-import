4057b71e504d2bd27e57272d5017fffe33b40572
Shekar Suman <shekharsuman97@gmail.com>
2024-03-09 15:57:37 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Vendor_CustomerImport',
+    'WundermanThompson_CustomerImport',
     __DIR__
 );
