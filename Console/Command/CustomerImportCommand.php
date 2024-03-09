bc66d0f59e0be18c6639213c01dec985c02b11b6
Shekar Suman <shekharsuman97@gmail.com>
2024-03-09 15:12:24 +0530

    Create CustomerImportCommand.php

---
+++ Console/Command/CustomerImportCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Vendor\Module\Console\Command;
+
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CustomerImportCommand extends Command
+{
+    protected static $defaultName = 'customer:import';
+
+    protected $customerRepository;
+
+    public function __construct(
+        CustomerRepositoryInterface $customerRepository
+    ) {
+        parent::__construct();
+        $this->customerRepository = $customerRepository;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Import customers from a CSV or JSON file')
+             ->setHelp('This command allows you to import customers from a CSV or JSON file')
+             ->addArgument('profile', InputArgument::REQUIRED, 'Profile name')
+             ->addArgument('source', InputArgument::REQUIRED, 'Source file');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $profile = $input->getArgument('profile');
+        $source = $input->getArgument('source');
+
+        // Implement logic to parse the input file based on the profile
+        // Extract customer data from the file
+        // Import customers using customer repository
+
+        $output->writeln('<info>Customers imported successfully.</info>');
+        return Command::SUCCESS;
+    }
+}
