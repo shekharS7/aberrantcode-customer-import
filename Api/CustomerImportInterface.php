c19286be7deb95d4a4a21bbf955a49313a96a696
Shekar Suman <shekharsuman97@gmail.com>
2024-03-11 11:25:05 +0530

    Update CustomerImportInterface.php

--- Api/CustomerImportInterface.php
+++ Api/CustomerImportInterface.php
@@ -7,20 +7,11 @@ use Symfony\Component\Console\Output\OutputInterface;
 interface CustomerImportInterface
 {
     /**
-     * Import customers from a CSV file.
+     * Import customers from a file.
      *
-     * @param string $filePath Path to the CSV file
+     * @param string $filePath Path to the file
      * @param OutputInterface $output Console output interface
      * @return void
      */
-    public function importCsv(string $filePath, OutputInterface $output): void;
-
-    /**
-     * Import customers from a JSON file.
-     *
-     * @param string $filePath Path to the JSON file
-     * @param OutputInterface $output Console output interface
-     * @return void
-     */
-    public function importJson(string $filePath, OutputInterface $output): void;
+    public function import(string $filePath, OutputInterface $output): void;
 }
