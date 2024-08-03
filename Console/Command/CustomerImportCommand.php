7ad1f1c23888a39431ea62cc60b0cfd09e0abc9a
Shekar Suman <shekharsuman97@gmail.com>
2024-08-03 21:23:35 +0530

    Update CustomerImportCommand.php

--- Console/Command/CustomerImportCommand.php
+++ Console/Command/CustomerImportCommand.php
@@ -1,6 +1,14 @@
 <?php
-
-namespace WundermanThompson\CustomerImport\Console\Command;
+/**
+ * Copyright © Shekhar Suman, 2024. All rights reserved.
+ * See COPYING.txt for license details.
+ * 
+ * @package     AberrantCode_CustomerImport
+ * @version     1.0.0
+ * @license     MIT License (http://opensource.org/licenses/MIT)
+ * @autor       Shekhar Suman
+ */
+namespace AberrantCode\CustomerImport\Console\Command;
 
 use Magento\Customer\Api\CustomerRepositoryInterface;
 use Symfony\Component\Console\Command\Command;
@@ -9,8 +17,8 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Filesystem;
-use WundermanThompson\CustomerImport\Model\CsvCustomerImport;
-use WundermanThompson\CustomerImport\Model\JsonCustomerImport;
+use AberrantCode\CustomerImport\Model\CsvCustomerImport;
+use AberrantCode\CustomerImport\Model\JsonCustomerImport;
 
 
 class CustomerImportCommand extends Command
