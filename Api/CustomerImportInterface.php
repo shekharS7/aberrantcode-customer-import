cce62869006ef60a85a24f21c52db6dc9aca74b9
Shekar Suman <shekharsuman97@gmail.com>
2024-08-03 21:22:07 +0530

    Update CustomerImportInterface.php

--- Api/CustomerImportInterface.php
+++ Api/CustomerImportInterface.php
@@ -1,6 +1,14 @@
 <?php
-
-namespace WundermanThompson\CustomerImport\Api;
+/**
+ * Copyright © Shekhar Suman, 2024. All rights reserved.
+ * See COPYING.txt for license details.
+ * 
+ * @package     AberrantCode_CustomerImport
+ * @version     1.0.0
+ * @license     MIT License (http://opensource.org/licenses/MIT)
+ * @autor       Shekhar Suman
+ */
+namespace AberrantCode\CustomerImport\Api;
 
 use Symfony\Component\Console\Output\OutputInterface;
 
